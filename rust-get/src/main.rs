ddc02253dd8e83363850393b74d8c13220c06b52
Rahul Badam <rahul.badam@juspay.in>
2024-04-22 23:51:49 +0530

    added get request

--- rust-get/src/main.rs
+++ rust-get/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    print!("Status: {}", res.status());
+    print!("body: {}", body);
+    print!("Headers: {:#?}", res.headers());
+    Ok(())
 }
